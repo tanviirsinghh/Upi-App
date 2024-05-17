31e5aaa74d1f3b1de29f702f22ee1f0b767abff0
Tanvir Singh <tanveersing559@gmail.com>
2024-05-17 23:15:23 +0530

    added route

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react'
+import { useNavigate } from "react-router-dom";
+function Home() {
+   const navigate = useNavigate()
+   useEffect(()=>{
+    navigate("/signup")
+   })
+
+  return (
+    <div>
+     
+   
+    </div>
+  )
+}
+
+export default Home
