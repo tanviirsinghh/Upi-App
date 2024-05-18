8b67f0cd0b7396757d14dd180a45ff163760a2b6
Tanvir Singh <tanveersing559@gmail.com>
2024-05-18 14:04:14 +0530

    final

--- frontend/src/components/Users.jsx
+++ frontend/src/components/Users.jsx
@@ -9,7 +9,7 @@ import {useNavigate} from "react-router-dom"
     const [users, setUsers] = useState([]);
     const[filter, setFilter] = useState('')
     useEffect(() => {
-        axios.get("http://localhost:3000/api/v1/user/bulk?filter=" + filter)
+        axios.get("https://upi-app.onrender.com/api/v1/user/bulk?filter=" + filter)
         .then((response) =>{
             setUsers(response.data.user)
         })
